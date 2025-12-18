f611a53e32fe30dca495887f68254dcefc0c7c1d
Swet Mrigank <swet.mrigank.2018@iimu.ac.in>
2025-12-18 04:55:21 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -60,7 +60,8 @@ class Dev(Configuration):
         "allauth.account",
         "allauth.socialaccount",
         "allauth.socialaccount.providers.google",
-        "rest_framework"
+        "rest_framework",
+        "rest_framework.authtoken"
     ]
 
     MIDDLEWARE = [
@@ -183,6 +184,16 @@ class Dev(Configuration):
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher']
+
+
+
+    REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
 
 #     LOGGING = {
 #     "version": 1,
